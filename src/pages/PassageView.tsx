6632df54f405b53815e65ff76193c9148f05ddf6
Michael Ekert <ekert.michael@gmail.com>
2025-10-22 15:55:31 +0200

    fix error with current date and usestate (unessesery)

--- src/pages/PassageView.tsx
+++ src/pages/PassageView.tsx
@@ -36,7 +36,6 @@ export default function PassagePlan() {
   const windLayerRef = useRef<any>(null);
 
   const [startDate, setStartDate] = useState(new Date().toISOString().slice(0, 16));
-  const [currentTimeText, setCurrentTimeText] = useState("");
   const [segments, setSegments] = useState<Segment[]>([]);
 
   useEffect(() => {
